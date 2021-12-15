59d21a748bbfe516c38350ee48138841dae5db94
Eeman1113 <eeman.majumder@gmail.com>
2021-12-15 21:04:36 +0530

    neural net basic added

---
+++ Neural networks/Neural_Net_basic.py
@@ -0,0 +1,38 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist  # load dataset
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()  # split into tetsing and training
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),  # input layer (1)
+    keras.layers.Dense(128, activation='relu'),  # hidden layer (2)
+    keras.layers.Dense(10, activation='softmax') # output layer (3)
+])
+
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)  # we pass the data, labels and epochs and watch the magic!
+
+
+#test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=1) 
+#print('Test accuracy:', test_acc)
+
+
+#predictions = model.predict(test_images)
