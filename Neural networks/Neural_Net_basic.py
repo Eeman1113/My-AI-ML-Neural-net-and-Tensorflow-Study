bf060f9bb5ecff84849a70bdf4f62d693f11f6c2
Eeman1113 <eeman.majumder@gmail.com>
2021-12-15 21:07:22 +0530

    neural net basic added

--- Neural networks/Neural_Net_basic.py
+++ Neural networks/Neural_Net_basic.py
@@ -34,5 +34,46 @@ model.fit(train_images, train_labels, epochs=10)  # we pass the data, labels and
 #test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=1) 
 #print('Test accuracy:', test_acc)
 
-
+#prediction part__________________________________________________________________________________________________
 #predictions = model.predict(test_images)
+
+
+
+
+#COLOR = 'white'
+#plt.rcParams['text.color'] = COLOR
+#plt.rcParams['axes.labelcolor'] = COLOR
+#
+#def predict(model, image, correct_label):
+#  class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+#               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+#  prediction = model.predict(np.array([image]))
+#  predicted_class = class_names[np.argmax(prediction)]
+#
+#  show_image(image, class_names[correct_label], predicted_class)
+#
+#
+#def show_image(img, label, guess):
+#  plt.figure()
+#  plt.imshow(img, cmap=plt.cm.binary)
+#  plt.title("Excpected: " + label)
+#  plt.xlabel("Guess: " + guess)
+#  plt.colorbar()
+#  plt.grid(False)
+#  plt.show()
+#
+#
+#def get_number():
+#  while True:
+#    num = input("Pick a number: ")
+#    if num.isdigit():
+#      num = int(num)
+#      if 0 <= num <= 1000:
+#        return int(num)
+#    else:
+#      print("Try again...")
+#
+#num = get_number()
+#image = test_images[num]
+#label = test_labels[num]
+#predict(model, image, label)
